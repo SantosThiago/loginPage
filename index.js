c3ee10203c65b083249c84e3df6c6337166cdf1c
Thiago <th_santos@id.uff.br>
2022-11-06 20:04:26 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let submit=document.getElementById('submit')
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  
+  const email = document.getElementById("input_email");
+  const pass = document.getElementById('input_pass');
+
+  if (!email.value)
+    email.style.border='2px solid red'
+  
+  if (!pass.value)
+    pass.style.border='2px solid red'
+
+  else if (email.value)
+    email.style.border= 'None'
+
+  else if (pass.value)
+    pass.style.border= 'None'
+});
